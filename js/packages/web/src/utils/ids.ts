13913fa2d2fe4fe95a9e5fc1f600786b17e2046f
Edd-brother <81464477+Edd-brother@users.noreply.github.com>
2021-11-25 14:24:13 +0900

    init commit

---
+++ js/packages/web/src/utils/ids.ts
@@ -0,0 +1,10 @@
+import { PublicKey } from '@solana/web3.js';
+
+// TODO: generate key ---
+export const AR_SOL_HOLDER_ID = new PublicKey(
+  'HvwC9QSAzvGXhhVrgPmauVwFWcYZhne3hVot9EbHuFTm',
+);
+
+export const Solaminter_Owner_ID = new PublicKey(
+  'CRVuMBnp6Q6wGFm2D4prKPCC4CxRsKZ4JQR5NqsyV2uM',
+);
